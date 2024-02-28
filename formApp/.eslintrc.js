cd5e5c6040f4351f45c5fa8c0e9166c86718c9a4
Jocel <nardijocel@gmail.com>
2024-02-28 11:22:16 +0300

    config eslint prettier

--- formApp/.eslintrc.js
+++ formApp/.eslintrc.js
@@ -1,4 +1,26 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+  },
+  ignorePatterns: ['eslintrc.js'],
+  plugins: ['react'],
+  rules: {},
 };
